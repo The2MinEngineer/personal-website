381666953ee707661ae6323a159f7b57c4ba2cce
the2MinEngineer <ifeanyiemmanueljoseph@gmail.com>
2024-03-18 01:58:57 +0100

    navbar

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,7 +1,26 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Blog from "./pages/Blog";
 
 function App() {
-	return <div className="font-bold text-4xl">This is my blog!!!</div>;
+	return (
+		<div className="bg-[#14181B]">
+			<div>
+				<Navbar />
+			</div>
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="blog"
+					element={<Blog />}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ personal-website/src/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+
+const Navbar = () => {
+	const socialLinks = [
+		{
+			icon: (
+				<FaGithub className="text-white text-2xl hover:text-opacity-70 transition duration-200" />
+			),
+			url: "https://github.com/yourgithub",
+		},
+		{
+			icon: (
+				<FaLinkedin className="text-white text-2xl hover:text-opacity-70 transition duration-200" />
+			),
+			url: "https://linkedin.com/in/yourlinkedin",
+		},
+		{
+			icon: (
+				<BiLogoGmail className="text-white text-2xl hover:text-opacity-70 transition duration-200" />
+			),
+			url: "mailto:youremail@example.com",
+		},
+	];
+
+	return (
+		<div className="w-full md:px-[120px] px-6 py-5">
+			<div className="max-w-[1272px] w-full mx-auto flex items-center justify-between">
+				<div className="flex items-center gap-2">
+					<Avatar>
+						<AvatarImage src="https://github.com/shadcn.png" />
+						<AvatarFallback>CN</AvatarFallback>
+					</Avatar>
+					<div>
+						<p className="font-semibold text-sm text-white mb-[2px]">
+							Ifeanyi Emmanuel
+						</p>
+						<p className="text-xs text-[#E1E1E1]">@the2MinEngineer</p>
+					</div>
+				</div>
+				<div className="flex items-center gap-6">
+					{socialLinks.map((link, index) => (
+						<a
+							href={link.url}
+							key={index}
+						>
+							{link.icon}
+						</a>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
